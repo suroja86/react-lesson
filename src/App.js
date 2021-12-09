3c0ed8e83347c5ac3cf31fa0796282ad0cccc153
Serhii Makarenko <suroja86@gmail.com>
2021-12-09 09:18:11 +0200

    buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import './css/App.css';
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile/Profile";
+import Button from "./components/Button/button";
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header />
-            <Navbar />
-            <Profile />
+        <div>
+            <div>
+                <Button
+                    name="Увійти"
+                    linearColorStop="#072468"
+                    colorHint="#0F49D3"
+                    event={login}
+                />
+            </div>
+            <div>
+                <Button
+                    name="Увійти через Google"
+                    linearColorStop="#FE3030"
+                    colorHint="#FE3030"
+                    event={loginGoogle}
+                />
+            </div>
         </div>
     );
+}
+
+const login = () => {
+    /*TODO*/
+}
+
+const loginGoogle = () => {
+    /*TODO*/
 }
 
 export default App;
--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import s from './css/button.css'
+import s from './button.module.css'
 
 const Button = (props) => {
     return(
         <button
             className={s.loginButton}
             style={{background: "linear-gradient(90deg, "+props.linearColorStop+" 3.64%, "+props.colorHint+" 100%"}}
+            onClick={props.event}
         >
             {props.name}
         </button>
